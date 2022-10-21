097023674a0c23b05090ffe98fb27c0a45d34af3
CNMoreno <mateomoreno123444@gmail.com>
2022-10-21 17:23:04 -0500

    firts commit

---
+++ src/App.jsx
@@ -0,0 +1,141 @@
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
+import Modal from "./components/Modal";
+import IconoNuevoGasto from "./img/nuevo-gasto.svg";
+import { v1 as uuidv1 } from "uuid";
+import LIstadoGastos from "./components/LIstadoGastos";
+import Filtros from "./components/Filtros";
+
+function App() {
+  const [presupuesto, setPresupuesto] = useState(
+    Number(localStorage.getItem("presupuesto") ?? 0)
+  );
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [animarModal, setAnimarModal] = useState(false);
+  const [gastos, setGastos] = useState(
+    localStorage.getItem("gastos")
+      ? JSON.parse(localStorage.getItem("gastos"))
+      : []
+  );
+
+  const [gastoEditar, setGastoEditar] = useState({});
+  const [filtro, setFiltro] = useState("");
+  const [gastosFiltrados, setGastosFiltrados] = useState([]);
+
+  useEffect(() => {
+    if (Object.keys(gastoEditar).length > 0) {
+      setModal(true);
+      setTimeout(() => {
+        setAnimarModal(true);
+      }, 200);
+    }
+  }, [gastoEditar]);
+
+  useEffect(() => {
+    localStorage.setItem("presupuesto", presupuesto ?? 0);
+  }, [presupuesto]);
+
+  useEffect(() => {
+    localStorage.setItem("gastos", JSON.stringify(gastos) ?? []);
+  }, [gastos]);
+
+  useEffect(() => {
+    if (filtro) {
+      const gastosFiltrados = gastos.filter(
+        (gasto) => gasto.categoria === filtro
+      );
+      setGastosFiltrados(gastosFiltrados);
+    }
+  }, [filtro]);
+
+  useEffect(() => {
+    const presupuestoLS = Number(localStorage.getItem("presupuesto") ?? 0);
+
+    if (presupuestoLS > 0) {
+      setPresupuesto(presupuestoLS);
+      setIsValidPresupuesto(true);
+    }
+  }, []);
+
+  const handleNuevoGasto = () => {
+    setModal(true);
+    setGastoEditar({});
+    setTimeout(() => {
+      setAnimarModal(true);
+    }, 200);
+  };
+
+  const guardarGasto = (gasto) => {
+    if (gasto.id) {
+      const gastosActualizados = gastos.map((gastoActual) =>
+        gastoActual.id === gasto.id ? gasto : gastoActual
+      );
+
+      setGastos(gastosActualizados);
+      setGastoEditar({});
+    } else {
+      gasto.id = uuidv1();
+      gasto.fecha = Date.now();
+      setGastos([...gastos, gasto]);
+    }
+
+    setModal(false);
+    setTimeout(() => {
+      setAnimarModal(false);
+    }, 200);
+  };
+
+  const eliminarGasto = (id) => {
+    const gastosAcutalizados = gastos.filter((gasto) => gasto.id !== id);
+
+    setGastos(gastosAcutalizados);
+  };
+
+  return (
+    <div className={modal ? "fijar" : ""}>
+      <Header
+        gastos={gastos}
+        setGastos={setGastos}
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+        isValidPresupuesto={isValidPresupuesto}
+        setIsValidPresupuesto={setIsValidPresupuesto}
+      />
+      {isValidPresupuesto && (
+        <>
+          <main>
+            <Filtros filtro={filtro} setFiltro={setFiltro} />
+            <LIstadoGastos
+              gastos={gastos}
+              setGastoEditar={setGastoEditar}
+              eliminarGasto={eliminarGasto}
+              filtro={filtro}
+              gastosFiltrados={gastosFiltrados}
+            />
+          </main>
+          <div className="nuevo-gasto">
+            <img
+              src={IconoNuevoGasto}
+              alt="Icono nuevo gasto"
+              onClick={handleNuevoGasto}
+            />
+          </div>
+        </>
+      )}
+
+      {modal && (
+        <Modal
+          setModal={setModal}
+          animarModal={animarModal}
+          setAnimarModal={setAnimarModal}
+          guardarGasto={guardarGasto}
+          gastoEditar={gastoEditar}
+          setGastoEditar={setGastoEditar}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
